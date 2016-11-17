07e871b69d1dca83f91ce17f1661c29e8d5e12c7
alu0100622492 <alu0100622492@ull.edu.es>
2016-11-17 19:06:27 +0000

    Creando pruebas TDD para la clase menu_edades y sus respectivas clases

---
+++ lib/dieta/menu_cuatro_ocho.rb
@@ -0,0 +1,9 @@
+module Dieta
+
+	class Menu_cuatro_ocho < Menu_edades
+	
+		def initialize()
+		super	
+		end	
+	end
+end
---
+++ lib/dieta/menu_edad.rb
@@ -0,0 +1,9 @@
+module Dieta
+	class Menu_edades < Dieta
+		
+		def initialize()
+		#@menus= Dieta::Lista_doble.new
+		end
+
+	end
+end
